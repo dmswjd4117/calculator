dbc1ac7e1c9c381f07be3e22150e6a2c47a3572a
dmswjd4117 <dmswjd4117@naver.com>
2021-06-19 16:18:44 +0900

    Create README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+## Java Calculator
+---
+
+<br>
+<br>
+
+<img src="img/1.jpg"  width="230" height="300">
+ 
+<br>
+<br>
+<img src="img/2.jpg"  width="230" height="300">
+ 
--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,6 @@ public class Main {
 		BasicLayout frame = new BasicLayout();
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 	    frame.setVisible(true);
-
-		ExpressionTree e = new ExpressionTree();
-		System.out.println(e.calculate("(-20+0)-7*8+2"));
 	}
 }
 
-
-//( 0   -   5   ×   8   ÷   2 )   -   7   ×   8   +   2   ÷   9   -   ( 6   ×   2 )
